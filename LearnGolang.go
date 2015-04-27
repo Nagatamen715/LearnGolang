0410bd53df7ef22932506f667bcd2719556d546e
Benjamin Hager <benjaminhager@gmail.com>
2015-04-27 13:20:21 -0700

    Finished BeyondHello
    Finished writing BeyondHello and corrected some typos in the other
    functions. Tested BeyondHello function and it seems to be working fine.

--- LearnGolang.go
+++ LearnGolang.go
@@ -1,3 +1,4 @@
+
 // Single line comment
 /* Multi-
    line comment*/
@@ -33,13 +34,13 @@ func beyondHello() {
 	x = 3		// Variable assignment.
 	// "Short" declarations use := to infer the type, declare and assign.
 	y := 4
-	sum, prod := learnMultiple(x,y)			// Function returns two values.
+	sum, prod := learnMultiple(x, y)			// Function returns two values.
 	fmt.Println("sum:", sum, "prod:", prod)	// Simple output.
 	learnTypes()
 }
 
 // Functions can have parameters and (multiple!) return values.
-func learnMultiple(x, y int) {
+func learnMultiple(x, y int) (sum, prod int) {
 	return x + y, x * y	// Return two values.
 }
 
@@ -48,8 +49,8 @@ func learnTypes() {
 	// Short declaration usually gives you want you want.
 	str := "Learn Go!"	// string type.
 	
-	s2 := 'A "raw" string literal
-	can include line breaks.'	// Same string type.
+	s2 := `A "raw" string 
+literal can include line breaks.`	// Same string type.
 	
 	// Non-ASCII literal. Go source is UTF-8.
 	g := 'Σ'	// rune type, an alias for int32, holds a unicode code point.
@@ -81,7 +82,7 @@ func learnTypes() {
 	// First argument is a slice to which we are appending. Commonly,
 	// the array variable is updated in place, as in example below.
 	s := []int{1, 2, 3}		// Result is a slice of length 3.
-	s := append(s, 4, 5, 6)	// Added 3 elements. Slice now has length of 6.
+	s = append(s, 4, 5, 6)	// Added 3 elements. Slice now has length of 6.
 	fmt.Println(s)	// Updated slice is now [1 2 3 4 5 6]
 	// To append another slice, instead of list of atomic elements we can
 	// pass a reference to a slice or a slice literal like this, with a 
@@ -89,4 +90,21 @@ func learnTypes() {
 	//appending them to slice s.
 	s = append(s, []int{7, 8, 9}...)	// Second argument is a slice literal.
 	fmt.Println(s)		// Updated slice is now [1 2 3 4 5 6 7 8 9]
+	
+	p, q := learnMemory()	// Declares p, q to be type pointed to int.
+	fmt.Println(*p, *q)		// * follows a pointer. This prints two ints.
+	
+	// Maps are a dynamically growable associative array type, like the
+	// hash or dictionary types of some other languages.
+	m := map[string]int{"three":3, "four":4}
+	m["one"] = 14195
+	
+	// Unused variables are an error in Go.
+	// The underbar lets you "use" a variable but discard its value.
+	_, _, _, _, _, _, _, _, _, _ = str, s2, g, f, u, pi, n, a3, s4, bs
+	
+	// Output of course counts as using a variable.
+	fmt.Println(s, c, a4, s3, d2, m)
+	
+	learnFlowControl() // Back in the flow
 }
